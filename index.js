1ca3508fad32585d29e3f9faaaaeb15c737277a5
thaivm-0609 <vuongthai95@gmail.com>
2025-01-09 16:25:37 +0700

    feat: init project

---
+++ index.js
@@ -0,0 +1,9 @@
+//CÚ PHÁP KHAI BÁO: var tenBien: kieuDuLieu = value; 
+//1. Kiểu dữ liệu cơ bản: Number/String/Boolean
+//1.1. Number (số)
+var a = 10;
+console.log(a);
+//1.2. String (chuỗi)
+var chuoi = 'day la chuoi';
+//1.3. Boolean (true/false)
+var isValid = false;
---
+++ index.ts
@@ -0,0 +1,11 @@
+//CÚ PHÁP KHAI BÁO: var tenBien: kieuDuLieu = value; 
+//1. Kiểu dữ liệu cơ bản: Number/String/Boolean
+//1.1. Number (số)
+var a:number = 10;
+console.log(a);
+//1.2. String (chuỗi)
+var chuoi:string = 'day la chuoi';
+//1.3. Boolean (true/false)
+var isValid:boolean = false;
+
+//2. Kiểu dữ liệu đặc biệt (Object, Array, tuple, union)
