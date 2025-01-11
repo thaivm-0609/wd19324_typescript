2e26c667a8bf3c46e22c6a2303b04de82aa32866
thaivm-0609 <vuongthai95@gmail.com>
2025-01-11 15:58:26 +0700

    feat: object array ...

--- index.js
+++ index.js
@@ -7,3 +7,53 @@ console.log(a);
 var chuoi = 'day la chuoi';
 //1.3. Boolean (true/false)
 var isValid = false;
+//2. Kiểu dữ liệu đặc biệt (Object, Array, tuple, union, any)
+//2.1. Object
+var objectSV = {
+    //key: value
+    maSV: 'PH12345',
+    hoTen: 'Vương Minh Thái',
+    email: 'thaivm2@fe.edu.vn',
+    age: 100,
+    status: true,
+};
+//2.2. Array (mảng): Có 2 cách để khai báo 1 mảng trong typescript
+//C1: dùng cặp ngoặc vuông []: 
+//var tenMang: kieuDuLieuCuaCacPhanTu[] = [item1, item2, ...];
+var testArray1 = [1, 2, 3, 4, 5];
+console.log(testArray1[2]);
+//C2: dùng từ khóa Array<kieuDuLieuCuaCacPhanTu> (angle-bracket)
+//var tenMang: Array<kieuDuLieuCuaCacPhanTu> = [];
+var testArray2 = ['Một', "Hai", "Ba"];
+//2.3. Tuple (là kiểu dữ liệu mới trong typescript)
+//khai báo kiểu dữ liệu cho từng phần tử ở trong mảng => biết trước số phần tử trong mảng
+var testTuple = [1, 'abc', false];
+//2.4. Union: có thể nhận một trong nhiều kiểu dữ liệu đều hợp lệ
+//var tenBien:kieuDuLieu1|kieuDuLieu2|...|kieuDuLieuX = value;
+var testUnion = "true";
+//kết hợp mảng và union
+//C1: dùng ngoặc vuông []
+var testUnionArray = [1, 2, 3, '4', '5'];
+//C2: dùng Array<> 
+var testUnionArray2 = [true, false, 1];
+//2.5. Any: không xác định trước kiểu dữ liệu => gán giá trị thuộc kiểu dữ liệu nào cũng đc
+//var tenBien:any = value;
+//2.6. Enum: enum tenBien{Value1, Value2, ...};
+var Role;
+(function (Role) {
+    Role[Role["Admin"] = 0] = "Admin";
+    Role[Role["User"] = 1] = "User";
+})(Role || (Role = {}));
+;
+var Gender;
+(function (Gender) {
+    Gender[Gender["Male"] = 0] = "Male";
+    Gender[Gender["Female"] = 1] = "Female";
+})(Gender || (Gender = {}));
+;
+//Ép kiểu trong typescript
+var input = 22222; //B1: ép kiểu về string => B2: dùng .length để lấy đc độ dài chuỗi
+//C1: angle-bracket <kieuDuLieuMuonEp>tenBien
+var count1 = input.length;
+//C2: sử dụng từ khóa as: (tenBien as kieuDuLieuMuonEp)
+var count2 = input.length;
--- index.ts
+++ index.ts
@@ -8,4 +8,57 @@ var chuoi:string = 'day la chuoi';
 //1.3. Boolean (true/false)
 var isValid:boolean = false;
 
-//2. Kiểu dữ liệu đặc biệt (Object, Array, tuple, union)
+//2. Kiểu dữ liệu đặc biệt (Object, Array, tuple, union, any)
+//2.1. Object
+var objectSV: { //khai báo kiểu dữ liệu của từng key trong object
+    //key: kiểu dữ liệu của key (KHÔNG ĐƯỢC BỎ)
+    //key?: kiểu dữ liệu của key (TÙY CHỌN, ko có cũng đc)
+    maSV: string,
+    hoTen: string,
+    email: string,
+    age: number,
+    status?: boolean
+} = {
+    //key: value
+    maSV: 'PH12345',
+    hoTen: 'Vương Minh Thái',
+    email: 'thaivm2@fe.edu.vn',
+    age: 100,
+    status: true,
+}
+
+//2.2. Array (mảng): Có 2 cách để khai báo 1 mảng trong typescript
+//C1: dùng cặp ngoặc vuông []: 
+//var tenMang: kieuDuLieuCuaCacPhanTu[] = [item1, item2, ...];
+var testArray1:number[] = [1,2,3,4,5];
+console.log(testArray1[2]);
+//C2: dùng từ khóa Array<kieuDuLieuCuaCacPhanTu> (angle-bracket)
+//var tenMang: Array<kieuDuLieuCuaCacPhanTu> = [];
+var testArray2:Array<string> = ['Một', "Hai", `Ba`];
+
+//2.3. Tuple (là kiểu dữ liệu mới trong typescript)
+//khai báo kiểu dữ liệu cho từng phần tử ở trong mảng => biết trước số phần tử trong mảng
+var testTuple:[number,string,boolean] = [1,'abc',false]
+
+//2.4. Union: có thể nhận một trong nhiều kiểu dữ liệu đều hợp lệ
+//var tenBien:kieuDuLieu1|kieuDuLieu2|...|kieuDuLieuX = value;
+var testUnion:number|string = "true";
+//kết hợp mảng và union
+//C1: dùng ngoặc vuông []
+var testUnionArray:(number|string)[] = [1,2,3,'4','5'];
+//C2: dùng Array<> 
+var testUnionArray2:Array<number|boolean> = [true,false,1];
+
+//2.5. Any: không xác định trước kiểu dữ liệu => gán giá trị thuộc kiểu dữ liệu nào cũng đc
+//var tenBien:any = value;
+
+//2.6. Enum: enum tenBien{Value1, Value2, ...};
+enum Role {Admin,User};
+enum Gender {Male,Female};
+
+//Ép kiểu trong typescript
+var input:any = 22222; //B1: ép kiểu về string => B2: dùng .length để lấy đc độ dài chuỗi
+//C1: angle-bracket <kieuDuLieuMuonEp>tenBien
+var count1:number = (<string>input).length;
+//C2: sử dụng từ khóa as: (tenBien as kieuDuLieuMuonEp)
+var count2:number = (input as string).length;
