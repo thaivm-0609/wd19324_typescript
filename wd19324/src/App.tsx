fa7b56904707f7c6082871dc40e54a476d8e3f93
thaivm-0609 <vuongthai95@gmail.com>
2025-01-18 16:03:38 +0700

    feat: init project react typescript

---
+++ wd19324/src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css'
+import { useRoutes } from 'react-router-dom'
+import Home from './pages/Home'
+import Register from './pages/Register';
+import NotFound from './pages/NotFound';
+
+const routeConfig = [
+  { //router home
+    path: '/', //khai báo url để truy cập vào chức năng đó
+    element: <Home/>, //khai báo component được hiển thị ra
+  },
+  { //router register
+    path: '/register',
+    element: <Register/>,
+  },
+  { //router not found
+    path: '*',
+    element: <NotFound/>
+  }
+]
+
+function App() {
+  const router = useRoutes(routeConfig);
+
+  return <div>{router}</div>
+}
+
+export default App
